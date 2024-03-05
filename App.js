0ed5c9a3fd2281988733dd782ac267f42aedb64a
shanharold <146119600+shanharold@users.noreply.github.com>
2024-03-06 00:08:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount(){
+    
+  }
+  fetchWeather(lat = 25, lon = 25){
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      });
+  }
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? <Text>Fetching The Weather</Text> : <Weather />}
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
